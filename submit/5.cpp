867fcea5aae32d166339208d76941efe8b8e55b9
limaotong <82311072@qq.com>
2026-06-28 13:12:30 +0800

    提交评测 #5 (题目 1001)

---
+++ submit/5.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<pii, int>
+#define pll pair<ll, ll>
+#define plll pair<pll, ll>
+#define pref static inline
+#define fi first
+#define se second
+using namespace std;
+namespace heffo_hard{
+    pref void solve(){
+        
+    }
+};
+
+signed main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    heffo_hard::solve();
+    return 0;
+}
+/*
+heffo_hard
+*/
