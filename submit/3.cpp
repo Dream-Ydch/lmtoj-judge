8c34cef782f57e71519b9d1fc3619f089f8ef8dc
limaotong <82311072@qq.com>
2026-06-28 11:23:11 +0800

    提交评测 #3 (题目 1001)

---
+++ submit/3.cpp
@@ -0,0 +1,57 @@
+/*--------------------------------------
+  Hi-Star 9.1.232 (2026.3.29) (2026.5.17)
+  From          :
+  ID            :
+  English Name  :
+  Chinese Name  :
+  Coding Time   :
+  Difficulty    :
+--------------------------------------*/
+
+// htoj.com.cn |  luogu.com.cn  | xmoj.tech | acwing.com | atcoder.jp
+//  etiger.vip | codeforces.com | hydro.ac  | vjudge.net
+
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define lll __int128
+#define pll pair<ll, ll>
+#define INF64 0x3f3f3f3f3f3f3f3f
+#define INF32 0x3f3f3f3f
+#define INF16 0x3f3f
+#define INF8 0x3f
+#define fill0(m) memset(m, 0, sizeof(m))
+#define fill0x3f(m) memset(m, INF8, sizeof(m))
+#define fill_1(m) memset(m, -1, sizeof(m))
+#define forab(a, b) for (ll i = a; i <= b; i++)
+#define rofba(b, a) for (ll i = b; i >= a; i--)
+#define forabj(a, b) for (ll j = a; j <= b; j++)
+#define rofbaj(b, a) for (ll j = b; j >= a; j--)
+#define endl '\n'
+using namespace std;
+
+// Constants
+const ld pie = acos(-1.0);
+
+// Structures
+
+// Variables
+
+// Functions (code)
+inline void fastcin() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+}
+inline ll lowbit(ll x) { return x & -x; };
+
+// Main code
+int main() {
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+  fastcin();
+  for(int i=1;i<=1145;i++) cout<<"CCFILOVEYOU";
+  return 0;
+}
