90fcc6678c89d6f9fc9c64463e12ac5971c7472c
Dream-Ydch <3594053943@qq.com>
2026-07-10 10:02:43 +0800

    提交评测 #79 (题目 1000)

---
+++ submit/79.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    pair<int, int> plus;
+    cin >> plus.first >> plus.second;
+    cout << plus.first + plus.second << '\n';
+    return 0;
+}
