2f62819f1ef23f885fad634f539f17cd58a6c4fc
Dream-Ydch <3594053943@qq.com>
2026-07-10 10:05:33 +0800

    提交评测 #81 (题目 1000)

---
+++ submit/81.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    pair<int, int> p;
+    cin >> p.first >> p.second;
+    cout << p.first + p.second << '\n';
+    return 0;
+}
