f850129ca9b197a2bec3ea4ed230e731e95f2987
Dream-Ydch <3594053943@qq.com>
2026-07-10 18:15:21 +0800

    提交评测 #135 (题目 1000)

---
+++ submit/135.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	i64 a, b;
+	cin >> a >> b;
+	i64 ans = a + b;
+	cout << ans << "\n";
+	return 0;
+}
