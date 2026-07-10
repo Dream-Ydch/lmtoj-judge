baed076a337a99c82cb5e9b4a5bb71b706e95a33
Dream-Ydch <3594053943@qq.com>
2026-07-10 09:54:24 +0800

    提交评测 #77 (题目 1000)

---
+++ submit/77.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    cout << 0 << '\n';
+    return 0;
+}
