1fc93758afda0761b8ace8e582946c731366e0a1
Dream-Ydch <3594053943@qq.com>
2026-07-10 20:20:16 +0800

    提交评测 #149 (题目 1001)

---
+++ submit/149.cpp
@@ -0,0 +1,14 @@
+// Copyright 2026 (C) Gary0
+// All rights reserved
+#define debug
+#include <bits/stdc++.h>
+#define inf 0x3f3f3f3f
+typedef long long LL;
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	cout << "CCFILOVEYOU";
+	return 0;
+}
