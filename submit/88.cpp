745a58616032f864544789a8eac09ffc1e847cf5
Dream-Ydch <3594053943@qq.com>
2026-07-10 12:34:13 +0800

    提交评测 #88 (题目 1000)

---
+++ submit/88.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+    #define  /*         */ /*        */ /*     */ /* */_000AAA a /*     */ /* */
+    #define  /*  */ /*        */ /*      */ /*      */ /*   */_000AAB b /*    */
+ /**/#define _000AAC  /*  */ /**/ /* */ /**/ /*         */ /*        */   cin   
+#define  /*   */ /**/ /**/ /*      */ /* */ /**/ /*   */ /*   */  _000AAD cout  
+#define  /*        */ /*  */ /**/   _000AAE     int /*       */ /*  */ /*     */
+ /*     */ /*     */#define  /* */ /*     */ /* */_000AAF  /*    */ /* */ main  
+ /* */#define  /*        */  _000AAG   namespace /*        */ /*        */ /* */
+ /**/#define _000AAH  /*          */ /*      */std /*    */ /*   */ /*      */  
+ /*    */ #define  /*  */ /*    */ /*         */ /*      */    _000AAI     using
+#define  /* */ /*       */ /*      */   _000AAJ   _000AAI _000AAG _000AAH ; /**/
+   #define  /*          */ /*      */ /*         */  _000AAK _000AAE _000AAF (){
+ /**/ /*         */ /* */#define _000AAL  /*      */ /* */_000AAE    _000AAA ,b;
+#define  /*      */_000AAM _000AAC     >>a>>b; /*     */ /*  */ /*     */ /*  */
+#define _000AAN  /*       */ /*    */_000AAD <<a+b; /*        */ /* */ /*  */   
+ /*    */ /*       */#define  /*  */ _000AAO  /*     */ /*     */ /*    */ /**/}
+#define _FINAL _000AAJ _000AAK _000AAL _000AAM _000AAN _000AAO /*          */   
+ /*     */ /*   */    _FINAL /*    */ /*        */ /*        */ /*          */
