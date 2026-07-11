019a3293b438d2f99a8e6681479342b1c3902fdf
Dream-Ydch <3594053943@qq.com>
2026-07-11 11:15:24 +0800

    提交评测 #200 (题目 1000)

---
+++ submit/200.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 50;
+typedef long long ll;
+int a[10000];
+signed main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
