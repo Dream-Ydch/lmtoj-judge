fff09a0302f37481e48aa6f98ce2caa2177a12df
Dream-Ydch <3594053943@qq.com>
2026-07-12 23:16:48 +0800

    提交评测 #253 (题目 1000)

---
+++ submit/253.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+
+inline void solve(){
+    int a,b;cin>>a>>b;cout<<a+b;
+}
+
+signed main(){
+    int t=1;
+    while(t--) solve();
+    return 0;
+}
