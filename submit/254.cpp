03f95040c30bc7cdbe130075da2c6dd70baec686
Dream-Ydch <3594053943@qq.com>
2026-07-12 23:17:44 +0800

    提交评测 #254 (题目 1001)

---
+++ submit/254.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+inline void solve(){
+    printf("CCFILOVEYOU");
+}
+
+signed main(){
+    int t=1145;
+    while(t--) solve();
+    return 0;
+}
