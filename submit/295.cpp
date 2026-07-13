4492b0967ec6cfafae3d32a81231429db92c1c34
Dream-Ydch <3594053943@qq.com>
2026-07-13 20:55:51 +0800

    提交评测 #295 (题目 1000)

---
+++ submit/295.cpp
@@ -0,0 +1,19 @@
+#include<bits/extc++.h>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+using pii=pair<int,int>;
+using ll=long long;
+using ld=long double;
+using i128=__int128;
+#define fi first
+#define se second
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    ll a,b;
+    cin>>a>>b;
+    cout<<a+b;
+	return 0;
+}
