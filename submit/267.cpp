978f6cb9133e1bdcba96b6b595c4d7b76b6fd6dd
Dream-Ydch <3594053943@qq.com>
2026-07-13 11:56:56 +0800

    提交评测 #267 (题目 1000)

---
+++ submit/267.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ft first
+#define sd second
+#define fs(i,x,y) for(int i=(x);i<=(y);i++)
+#define fj(i,x,y) for(int i=(x);i>=(y);i--)
+#define len(s) s.length()
+#define mht(a,b,x,y) (fabs(x-a)+fabs(y-b))
+#define juli(a,b,x,y) (sqrt((x-a)*(x-a)+(y-b)*(y-b)))
+#define numb(c) (c-'0')
+#define pii pair<int,int>
+signed main(){
+	std::ios::sync_with_stdio(false);
+	cin.tie(0); 
+	cout.tie(0);
+	return 0;
+}
