ea12719b7e3f52ab2cb23fb62b5060bcf050e203
Dream-Ydch <3594053943@qq.com>
2026-07-13 20:58:04 +0800

    提交评测 #296 (题目 1000)

---
+++ submit/296.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+//十年OI一场空，不开long long见祖宗
+int a , b;
+int main(){
+	//freopen(".in" , "r" , stdin);
+	//freopen(".out" , "w" , stdout);
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
