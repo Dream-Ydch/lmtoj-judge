971a140fa7643d97d912bf6053189b47c2d00d02
Dream-Ydch <3594053943@qq.com>
2026-07-20 23:04:36 +0800

    提交评测 #411 (题目 1004)

---
+++ submit/411.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	vector<int>a;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		int x;
+		cin>>x;
+		a.push_back(x);
+	}
+	sort(a.begin,a.end();
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
