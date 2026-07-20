7fa9a5455e4a629dc47d4b16db5286f3a22f79a8
Dream-Ydch <3594053943@qq.com>
2026-07-20 17:03:07 +0800

    提交评测 #399 (题目 1000)

---
+++ submit/399.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main() {
+    int a, b;
+    cin >> a >> b;
+    int c[105];
+    cout << c[a] << '\n';
+    return 0;
+}
