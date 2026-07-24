e2b66a685a6233987d0800b7ce9b4b4b551d911b
Dream-Ydch <3594053943@qq.com>
2026-07-24 21:03:05 +0800

    提交评测 #485 (题目 1000)

---
+++ submit/485.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a,b;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
