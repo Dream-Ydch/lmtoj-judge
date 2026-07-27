5044b9de3c48bc69f6ff62de782a12c130bdae3a
Dream-Ydch <3594053943@qq.com>
2026-07-27 20:50:49 +0800

    提交评测 #499 (题目 1001)

---
+++ submit/499.cpp
@@ -0,0 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+for(int i=1;i<=(172+218+81457+178+19+37851+174812+83591014-128941294)*0+1145;i++)cout<<"CCFILOVEYOU"<<(i==1145?'\n':' ');
+}
