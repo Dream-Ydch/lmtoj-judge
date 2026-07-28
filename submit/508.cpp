aef6f4c37364f666c11c3d8aa8c78862e9f6abfa
Dream-Ydch <3594053943@qq.com>
2026-07-28 16:34:21 +0800

    提交评测 #508 (题目 1005)

---
+++ submit/508.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, v[1005], w[1005], f[10005];
+
+signed main() {
+    cin >> m >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> v[i] >> w[i];
+    for (int i = 1; i <= n; i++)
+        for (int j = m; j >= v[i]; j--)
+            f[j] = max(f[j], f[j - v[i]] + w[i]);
+    cout << f[m] << '\n';
+    return 0;
+}
